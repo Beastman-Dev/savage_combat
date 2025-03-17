5edc5f9c8281a4ef3f66ddff35207463bac06753
Beastman-dev <wmeastman@gmail.com>
2025-03-17 15:49:42 -0400

    Updated unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -2,12 +2,27 @@
 
 from main import *
 
-def test_get_data():
+def player_creation():
     player = Player("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
-    assert player.get_data() == ("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
-    opponent = Combatant("Opponent", 6, 6, 6, 6, 6, 2)
-    assert opponent.get_data() == ("Opponent", 6, 6, 6, 6, 6, 2)
+    assert (player.name, player.agility, player.smarts, player.spirit, player.strength, player.vigor, player.athletics, player.fighting, player.shooting, player.armor_value) == ("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
 
-def test_roll_die():
+def opponent_creation():
+    opponent = Combatant("Opponent", 6, 6, 6, 6, 6, 2)
+    assert (opponent.name, opponent.spirit, opponent.vigor, opponent.athletics, opponent.fighting, opponent.shooting, opponent.armor_value) == ("Opponent", 6, 6, 6, 6, 6, 2)
+
+def combat_creation():
     player = Player("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
-    assert player.roll_die(6) in range(1, 7)
+    opponent = Combatant("Opponent", 6, 6, 6, 6, 6, 2)
+    combat = Combat(player, opponent)
+    assert (combat.player.name, combat.opponent.name) == ("Player", "Opponent")
+    assert combat.roll_die(4) > 0
+    assert combat.result_selection(6) > 0
+
+if __name__ == "__main__":
+    print("Testing player creation...")
+    print(f"   Errors: {player_creation()}")
+    print(f"Testing opponent creation...")
+    print(f"   Errors: {opponent_creation()}")
+    print(f"Testing combat creation...")
+    print(f"   Errors: {combat_creation()}")
+    print("All tests passed!")
