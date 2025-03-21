bd0ca17a78db2714d83558e33f8c3f54c6750512
Beastman-dev <wmeastman@gmail.com>
2025-03-21 15:33:01 -0400

    Updated unit tests

--- main.py
+++ main.py
@@ -87,8 +87,7 @@ class Creature:
     # Function to call for all damage rolls which automatically adds Strength for melee attacks
     # Takes in attack_type, dice_count, dice_sides, and an optional modifier which defaults to 0
     # Returns the total damage dealt
-    def damage_roll(self, attack_type: str, dice_count: int, dice_sides: int, modifier: int = 0) -> int:
-        
+    def damage_roll(self, attack_type: str, dice_count: int, dice_sides: int, modifier: int = 0) -> int:        
         total_damage = self.roll_dice(dice_count, dice_sides)
         if attack_type == "melee":
             total_damage += self.roll_die(self.strength, True)
@@ -98,7 +97,7 @@ class Creature:
     # Function to call for initiating an attack and resolving the results
     # Takes in the target, attack_type, and any additional parameters
     # Returns the damage value modified by armor and armor piercing
-    def attack(self, target, attack_method: str, adjacent: bool = False) -> str:
+    def attack(self, target, attack_method: str, adjacent: bool = False) -> int:
         attack_type = self.attacks[attack_method][0]
         damage = self.attacks[attack_method][1]
         # Reduce target's armor value by the armor piercing value of the attack
@@ -109,7 +108,7 @@ class Creature:
         attack_result = self.attack_roll(target, attack_type, adjacent)
         # If result is 0, return "Miss"
         if attack_result == 0:
-            return "Miss"
+            return -1
         # Calculate the damage dealt
         damage_dealt = self.damage_roll(attack_type, damage[0], damage[1], damage[2])
         # Add 1d6 bonus damage if attack result was a raise
@@ -122,20 +121,26 @@ class Creature:
             damage_dealt = 0
         return damage_dealt
 
-    def apply_damage(self, damage: int) -> str:
-        wounds = math.floor((damage - self.toughness) / 4)
-        self.wounds += wounds
-        if self.wounds >= 4:
-            self.status = "Incapacitated"
-        elif wounds == 0 and self.status == "Shaken":
-            self.wounds += 1
-            if self.wounds >= 4:
-                self.status = "Incapacitated"
-        elif wounds >= 0:
-            self.status = "Shaken"
-        if self.wounds >=1 and self.status == "Uninjured":
-            self.status = "Wounded"
-        return self.status
+    # Function to apply damage to a creature and update their status
+    # Takes in target and the amount of damage dealt
+    # Returns the updated status of the creature
+    def apply_damage(self, target, damage: int) -> str:
+        if damage < target.toughness:
+            return target.status
+        wounds = math.floor((damage - target.toughness) / 4)
+        target.wounds += wounds
+        if target.wounds >= 4:
+            target.status = "Incapacitated"
+        elif wounds == 0 and target.status == "Shaken":
+            target.wounds += 1
+            target.status = "Wounded"
+            if target.wounds >= 4:
+                target.status = "Incapacitated"
+        elif wounds > 0:
+            target.status = "Wounded"
+        elif wounds == 0 and target.status == "Uninjured":
+            target.status = "Shaken"
+        return target.status
 
 class Player(Creature):
     def __init__(self, name: str, agility: int, smarts: int, spirit: int, strength: int, vigor: int, athletics: int, fighting: int, shooting: int, armor_value: int) -> None:
@@ -208,37 +213,22 @@ class Game:
         armor = input("Armor Value: ")
         self.opponent: Creature = Creature(name, spirit, vigor, athletics, fighting, shooting, armor)
 
-    def create_combat(self):
-        combat = Combat(self.player, self.opponent)
-        return combat
 
-#creature = Creature(name, spirit, strength, vigor, athletics, fighting, shooting, armor)
-player_fighter: Creature = Creature("Player Fighter", 6, 6, 6, 6, 6, 6, 2)
-goblin_grunt: Creature = Creature("Goblin Grunt", 6, 4, 6, 6, 6, 8)
-goblin_grunt.add_attack("Short Spear", "melee", (1, 4, 0), 0)
-goblin_grunt.add_attack("Short Bow", "ranged", (2, 6, 0), 0)
-player_fighter.add_attack("Long Sword", "melee", (2, 6, 0), 0)
-
-print(player_fighter.roll_wild(player_fighter.fighting))
-
-# print(f"Player attacks goblin: {player_fighter.attack_roll(goblin_grunt, "melee")}!")
-# print(f"Player damages goblin: {player_fighter.damage_roll("melee", 1, 8)}!")
-# print(f"Goblin attacks player: {goblin_grunt.attack_roll(player_fighter, 'melee')}!")
-# print(f"Goblin damages player: {goblin_grunt.damage_roll("melee", 1, 4)}!")
-# print(f"Goblin status: {goblin_grunt.status}!")
-# print(f"Player status: {player_fighter.status}!")
-# print(f"Goblin wounds: {goblin_grunt.wounds}!")
-# print(f"Player wounds: {player_fighter.wounds}!")
-
+# player = Player("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
+# enemy = Creature("enemy", 6, 6, 6, 6, 6, 6, 0)
 # for i in range(10):
-#     print(f"Rolling a d4: {Die(4).roll_die()}")
+#     print(player.attack(enemy, "unarmed"))
+#     input()
 
-# game = Game()
-# game.player_inputs()
-# game.opponent_inputs()
-# combat = game.create_combat()
-# print(combat.player.name)
-# print(combat.opponent.name)
+
+## creature = Creature(name, spirit, strength, vigor, athletics, fighting, shooting, armor)
+# player_fighter: Creature = Creature("Player Fighter", 6, 6, 6, 6, 6, 6, 2)
+# goblin_grunt: Creature = Creature("Goblin Grunt", 6, 4, 6, 6, 6, 8)
+# goblin_grunt.add_attack("Short Spear", "melee", (1, 4, 0), 0)
+# goblin_grunt.add_attack("Short Bow", "ranged", (2, 6, 0), 0)
+# player_fighter.add_attack("Long Sword", "melee", (2, 6, 0), 0)
+
+# player_fighter.attack(goblin_grunt, "Long Sword")
 
 # Step 1: Initialize game and collect player inputs
 # Step 2: Display current values and prompt to start combat loop
--- unit_tests.py
+++ unit_tests.py
@@ -6,23 +6,68 @@ def player_creation():
     player = Player("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
     assert (player.name, player.agility, player.smarts, player.spirit, player.strength, player.vigor, player.athletics, player.fighting, player.shooting, player.armor_value) == ("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
 
-def opponent_creation():
-    opponent = Combatant("Opponent", 6, 6, 6, 6, 6, 2)
-    assert (opponent.name, opponent.spirit, opponent.vigor, opponent.athletics, opponent.fighting, opponent.shooting, opponent.armor_value) == ("Opponent", 6, 6, 6, 6, 6, 2)
+def enemy_creation():
+    enemy = Creature("enemy", 6, 6, 6, 6, 6, 6, 2)
+    assert (enemy.name, enemy.spirit, enemy.strength, enemy.vigor, enemy.athletics, enemy.fighting, enemy.shooting, enemy.armor_value) == ("enemy", 6, 6, 6, 6, 6, 6, 2)
 
-def combat_creation():
+def add_attack():
+    enemy = Creature("enemy", 6, 6, 6, 6, 6, 6, 2)
+    enemy.add_attack("Short Spear", "melee", (1, 4, 0), 0)
+    assert enemy.attacks["Short Spear"] == ("melee", (1, 4, 0), 0)
+
+def roll_die():
+    enemy = Creature("enemy", 6, 6, 6, 6, 6, 6, 2)
+    assert enemy.roll_die(6) > 0
+
+def roll_dice():
+    enemy = Creature("enemy", 6, 6, 6, 6, 6, 6, 2)
+    assert enemy.roll_dice(6, 2) > 0
+
+def roll_wild():
+    enemy = Creature("enemy", 6, 6, 6, 6, 6, 6, 2)
+    assert enemy.roll_wild(6) > 0
+
+def attack_roll():
     player = Player("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
-    opponent = Combatant("Opponent", 6, 6, 6, 6, 6, 2)
-    combat = Combat(player, opponent)
-    assert (combat.player.name, combat.opponent.name) == ("Player", "Opponent")
-    assert combat.roll_die(4) > 0
-    assert combat.result_selection(6) > 0
+    enemy = Creature("enemy", 6, 6, 6, 6, 6, 6, 2)
+    assert player.attack_roll(enemy, "melee") in range(0,3)
+
+def damage_roll():
+    player = Player("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
+    enemy = Creature("enemy", 6, 6, 6, 6, 6, 6, 2)
+    assert player.damage_roll("melee", 1, 4) > 0
+
+def attack():
+    player = Player("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
+    enemy = Creature("enemy", 6, 6, 6, 6, 6, 6, 0)
+    assert player.attack(enemy, "unarmed") >= -1
+    
+def apply_damage():
+    player = Player("Player", 10, 6, 8, 6, 6, 8, 10, 8, 2)
+    enemy = Creature("enemy", 6, 6, 6, 6, 6, 6, 0)
+    assert player.apply_damage(enemy, 5) == "Shaken"
+    assert player.apply_damage(enemy, 9) == "Wounded"
+    assert player.apply_damage(enemy, 21) == "Incapacitated"
 
 if __name__ == "__main__":
     print("Testing player creation...")
     print(f"   Errors: {player_creation()}")
-    print(f"Testing opponent creation...")
-    print(f"   Errors: {opponent_creation()}")
-    print(f"Testing combat creation...")
-    print(f"   Errors: {combat_creation()}")
+    print(f"Testing enemy creation...")
+    print(f"   Errors: {enemy_creation()}")
+    print(f"Testing add_attack...")
+    print(f"   Errors: {add_attack()}")
+    print(f"Testing roll_die...")
+    print(f"   Errors: {roll_die()}")
+    print(f"Testing roll_dice...")
+    print(f"   Errors: {roll_dice()}")
+    print(f"Testing roll_wild...")
+    print(f"   Errors: {roll_wild()}")
+    print(f"Testing attack_roll...")
+    print(f"   Errors: {attack_roll()}")
+    print(f"Testing damage_roll...")
+    print(f"   Errors: {damage_roll()}")
+    print(f"Testing attack...")
+    print(f"   Errors: {attack()}")
+    print(f"Testing apply_damage...")
+    print(f"   Errors: {apply_damage()}")
     print("All tests passed!")
